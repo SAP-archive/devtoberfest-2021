c6b7960d1a2d7d1c1aaf0a32d12278bdd0fe1837
Thomas Jung <thomas.jung@sap.com>
2021-10-08 17:14:58 -0400

    Update build.js

--- actions/build.js
+++ actions/build.js
@@ -71,7 +71,7 @@ const main = async _ => {
 }
 
 function convertCSV2JSON(text) {
-    let array = text.split("\r")
+    let array = text.split("\n\r")
     let result = []
     let headers = array[0].split(",")
     console.log(array)
